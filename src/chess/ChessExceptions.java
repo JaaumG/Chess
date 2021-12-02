bdc9aedbc80a8b58f4ac4d653093bfdbfbe65a1e
João Guilherme <jaaumgui@gmail.com>
2021-12-02 15:16:01 -0300

    UI, Torre...

---
+++ src/chess/ChessExceptions.java
@@ -0,0 +1,12 @@
+package chess;
+
+import boardgame.BoardException;
+
+public class ChessExceptions extends BoardException{
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExceptions (String msg) {
+		super(msg);
+	}
+
+}
